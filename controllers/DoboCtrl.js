8e902ed3a63e9653cbbbdfcac13eba87645785dd
ruddls00114 <kkmm114@gmail.com>
2018-09-26 15:14:09 +0900

    서울도보 예약 url 추가

--- controllers/DoboCtrl.js
+++ controllers/DoboCtrl.js
@@ -92,9 +92,9 @@ exports.getDetail = async (req, res, next) => {
     
     temp = await doboModel.getDetail(reqData.dobo_idx);
     temp.review = await doboModel.getReview(reqData.dobo_idx);
-    const {idx, title, intro, content, image, course, bgi } = temp;
-    result.dobo =  {idx, title, intro, content, image, course, bgi };
-    
+    const {idx, title, intro, content, image, cos_code } = temp;
+    result.dobo =  {idx, title, intro, content, image };
+    result.dobo.cos_url = 'http://dobo.visitseoul.net/web/_kr/webReservation/new_request_step1.do?cos_code=' + cos_code ;
     result.review = temp.review;
 
     // result.dobo.tourlist =tourlist;
